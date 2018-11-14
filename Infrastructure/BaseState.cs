cb5a9a088987c8e93334cab5a65e5e0cea76d35c
Rahul Joseph <rahuljoseph11@gmail.com>
2018-11-14 15:38:24 +0100

    beginning

---
+++ Infrastructure/BaseState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+namespace HorrorAdventure.Infrastructure
+{
+    [Serializable]
+    public abstract class BaseState
+    {
+        protected int buildID;
+        protected string stateName;
+        protected bool bCompleted;
+        protected BaseState nextState;
+
+        #region Getters and Setters
+        public int BuildID { get => buildID; set => buildID = value; }
+        public string StateName { get => stateName; set => stateName = value; }
+        public bool Completed { get => bCompleted; set => bCompleted = value; }
+        #endregion
+
+        /// <summary>
+        /// Updates states and entities relevant to this state. Also monitors whether this 
+        /// state is completed and we can move on to the next one
+        /// </summary>
+        public abstract void Update();
+
+        public abstract void Render(ContentManager content, SpriteBatch sbatch);
+    }
+}
---
+++ Infrastructure/MainMenuState.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
+using Microsoft.Xna.Framework;
+
+namespace HorrorAdventure.Infrastructure
+{
+    public class MainMenuState : BaseState
+    {
+        private Texture2D bkg;
+        private Texture2D startButton;
+        private Texture2D quitButton;
+
+        #region Getters and Setters
+        public Texture2D Background { get => bkg; set => bkg = value; }
+        public Texture2D StartButton { get => startButton; set => startButton = value; }
+        public Texture2D QuitButton { get => quitButton; set => quitButton = value; }
+        #endregion
+
+        public MainMenuState()
+        {
+            buildID = 0;
+            bCompleted = false;
+            stateName = "Main Menu";
+        }
+
+        public override void Render(ContentManager content, SpriteBatch sbatch)                                                      
+        {
+            this.bkg = content.Load<Texture2D>("background");
+            sbatch.Begin();
+            sbatch.Draw(bkg, Vector2.Zero);
+            sbatch.End();
+        }
+
+        public override void Update()
+        {
+            Console.Write("updating...");
+        }
+        
+        #region Helper functions
+        
+        #endregion
+    }
+}
